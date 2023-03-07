f31f448fd99a657681b6b48e8376b48c51bbc44c
Alexander Hardwell <lexwell7@yahoo.com>
2023-03-06 20:09:47 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const heroText = document.getElementById("hero-text")
+const heroMeContainer = document.getElementById("hero-me-container")
+const about = document.getElementById("about")
+const navOptionEl = document.querySelectorAll('.nav-option')
+const slidingWrapper = document.querySelector('.slider')
+const button = document.getElementById("send-button")
+
+button.addEventListener("click",()=>{
+    console.log("messss");
+})
+
+
+
+
+heroText.addEventListener('click',()=>{
+heroText.classList.toggle("hero-text-hide")
+heroMeContainer.classList = "hero-me-container-active"
+
+})
+
+heroMeContainer.addEventListener('click',()=>{
+    heroMeContainer.classList = "hero-me-container"
+    heroText.classList = "hero-text"
+})
+
+
+navOptionEl.forEach(function(option,index){
+    option.addEventListener('click', function(){
+        slidingWrapper.style.transform = `translateY(${index * -100}vh)`
+        // navOptionEl[1].classList.toggle("testMe:hover","testMe:focus")
+     
+    })
+})
+
